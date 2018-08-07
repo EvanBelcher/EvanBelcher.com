59fbb864a48378c46f59301e9ed069c38db72e5f
Evan Belcher <evanbelcher3@gmail.com>
2018-08-07 15:22:13 -0700

    Updated for 2018 - Content updates for 2018 - Added js to make all external links open in a new tab

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+//Add target="_blank" to all external links
+$('a').each(function() {
+	var a = new RegExp('/' + window.location.host + '/');
+	if (!a.test(this.href)) {
+	   $(this).attr("target","_blank");
+	}
+ });
+
 //Projects modal
 var projects = '';
 
